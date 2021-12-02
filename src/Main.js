e8e904725cbb182de75e1db41ab6a03094201515
jamiguel23 <josematthewmiguel@gmail.com>
2021-12-02 15:04:24 -0800

    added form to main.js

--- src/Main.js
+++ src/Main.js
@@ -2,6 +2,8 @@ import { Component } from 'react';
 import HornedBeasts from './HornedBeasts.js';
 import Row from "react-bootstrap/Row"
 import Container from 'react-bootstrap/Container';
+import  Form  from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
 
 
 class Main extends Component{
@@ -10,6 +12,18 @@ class Main extends Component{
       <>
       <main>
         <h1> {this.props.title}</h1>
+        <Form>
+          <Form.Group className="mb-3" controlId="Horns">
+            <Form.Label>Number of Horns</Form.Label>
+            <Form.Control type="text" placeholder="Enter email" />
+            <Form.Text className="text-muted">
+              We'll never share your email with anyone else.
+            </Form.Text>
+          </Form.Group>
+            <Button variant="primary" type="submit">
+              Submit
+            </Button>
+        </Form>
 
       <div>
     
