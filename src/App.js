a40f176c633e3091e0004a40c625e725f0e3a027
jamiguel23 <josematthewmiguel@gmail.com>
2021-12-01 16:32:03 -0800

    data passed as prop from app to main

--- src/App.js
+++ src/App.js
@@ -2,21 +2,17 @@ import React, { Component } from 'react';
 import Main from './Main.js';
 import Footer from './Footer.js';
 import Header from './Header.js';
-
+import data from './data.json';
+import SelectedBeast from './SelectedBeast.js';
 
 export default class App extends Component {
   render() {
     return (
       <div>
         <Header />
-
-       
-
-          <Main />
-
-          
         
-          <Footer />
+        <Main data={data} />
+        <Footer />
        
       </div>
     )
---
+++ src/SelectedBeast.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+import React, { Component } from 'react'
+
+export default class SelectedBeast extends Component {
+  render() {
+    return (
+      <div>
+        <p>Go Dawgs</p>
+        
+      </div>
+    )
+  }
+}
